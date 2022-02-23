db2deae27040e114c2bdb82e5c12348e002e8469
Edy Segura <edysegura@gmail.com>
2022-02-23 07:40:32 -0300

    refactor: timer

--- src/assets/timer.js
+++ src/assets/timer.js
@@ -1,7 +1,7 @@
 let seconds = 0
 let timerId
 
-function toggleTimer() {
+function toggleTimer(callback) {
   if (timerId) {
     timerId = clearInterval(timerId)
     return
@@ -9,22 +9,22 @@ function toggleTimer() {
 
   timerId = setInterval(() => {
     ++seconds
-    updateDisplay(secondsToHms(seconds))
+    callback(seconds)
   }, 1000)
 }
 
 function stopTimer() {
   timerId = clearInterval(timerId)
   seconds = 0
-  updateDisplay('00:00:00')
+  callback('00:00:00')
 }
 
-function secondsToHms(elapsedSeconds) {
-  const h = Math.floor(elapsedSeconds / 3600)
-  const m = Math.floor((elapsedSeconds % 3600) / 60)
-  const s = Math.floor((elapsedSeconds % 3600) % 60)
+export function secondsToHms(elapsedSeconds) {
+  const hours = Math.floor(elapsedSeconds / 3600)
+  const minutes = Math.floor((elapsedSeconds % 3600) / 60)
+  const seconds = Math.floor((elapsedSeconds % 3600) % 60)
 
   const pad = (value) => (value < 10 ? '0' + value : value)
 
-  return pad(h) + ':' + pad(m) + ':' + pad(s)
+  return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds)
 }
