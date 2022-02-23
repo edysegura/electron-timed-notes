b75658f06ec950aa979f4c9cdce779c4fcfe889f
Edy Segura <edysegura@gmail.com>
2022-02-22 21:25:43 -0300

    feat: convert seconds to hh:mm:ss

--- src/assets/app.js
+++ src/assets/app.js
@@ -9,7 +9,7 @@ function toggleTimer() {
 
   timerId = setInterval(() => {
     ++seconds
-    updateDisplay(seconds)
+    updateDisplay(getElapsedTime(seconds))
   }, 1000)
 }
 
@@ -32,6 +32,23 @@ function setPlayButton() {
 function setStopButton() {
   const btn = document.getElementById('btn-stop')
   btn?.addEventListener('click', () => stopTimer())
+}
+
+function getElapsedTime(elapsedSeconds) {
+  let hours = Math.floor(elapsedSeconds / 3600) // get hours
+  let minutes = Math.floor((elapsedSeconds - hours * 3600) / 60) // get minutes
+  let seconds = elapsedSeconds - hours * 3600 - minutes * 60 //  get seconds
+  // add 0 if value < 10; Example: 2 => 02
+  if (hours < 10) {
+    hours = '0' + hours
+  }
+  if (minutes < 10) {
+    minutes = '0' + minutes
+  }
+  if (seconds < 10) {
+    seconds = '0' + seconds
+  }
+  return hours + ':' + minutes + ':' + seconds // Return is HH : MM : SS
 }
 
 setPlayButton()
