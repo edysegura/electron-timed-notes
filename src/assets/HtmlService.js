9d1d3a301c61ff5610adf8a9769629a05a7561d5
Edy Segura <edysegura@gmail.com>
2022-02-24 21:23:03 -0300

    refactor: move DOM handling to HtmlService module

--- src/assets/HtmlService.js
+++ src/assets/HtmlService.js
@@ -1,10 +1,34 @@
 export default class HtmlService {
-  constructor() {
+  #timer
+
+  constructor(ElapsedTimer) {
+    this.#timer = new ElapsedTimer(this.updateDisplay)
+    this.init()
+  }
+
+  init() {
     this.setForm()
+    this.setPlayButton()
+    this.setStopButton()
   }
 
   setForm() {
     const form = document.querySelector('form')
     form.addEventListener('submit', (event) => event.preventDefault())
+  }
+
+  updateDisplay(time) {
+    const timerElement = document.querySelector('.time')
+    timerElement.textContent = time
+  }
+
+  setPlayButton() {
+    const btn = document.getElementById('btn-play')
+    btn?.addEventListener('click', () => this.#timer.toggleTimer())
+  }
+
+  setStopButton() {
+    const btn = document.getElementById('btn-stop')
+    btn?.addEventListener('click', () => this.#timer.stopTimer())
   }
 }
--- src/assets/app.js
+++ src/assets/app.js
@@ -1,23 +1,5 @@
 import ElapsedTimer from './ElapsedTimer.js'
 import HtmlService from './HtmlService.js'
 
-const elapsedTimer = new ElapsedTimer(updateDisplay)
-new HtmlService()
+new HtmlService(ElapsedTimer)
 
-function updateDisplay(time) {
-  const timerElement = document.querySelector('.time')
-  timerElement.textContent = time
-}
-
-function setPlayButton() {
-  const btn = document.getElementById('btn-play')
-  btn?.addEventListener('click', () => elapsedTimer.toggleTimer())
-}
-
-function setStopButton() {
-  const btn = document.getElementById('btn-stop')
-  btn?.addEventListener('click', () => elapsedTimer.stopTimer())
-}
-
-setPlayButton()
-setStopButton()
