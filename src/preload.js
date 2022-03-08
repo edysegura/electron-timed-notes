33a0e0b6a838b0edc1cfc8800959ef3ceaeb7c4d
Edy Segura <edysegura@gmail.com>
2022-03-07 23:02:25 -0300

    feat: implement clipboard feature

--- src/preload.js
+++ src/preload.js
@@ -1,5 +1,20 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
+const { clipboard } = require('electron')
+
 window.addEventListener('DOMContentLoaded', () => {
   console.log('Ready for action!')
+  setClipboardButton()
 })
+
+function setClipboardButton() {
+  const button = document.getElementById('btn-clipboard')
+  button.addEventListener('click', () => {
+    clipboard.writeText(getNotes())
+  })
+}
+
+function getNotes() {
+  const textarea = document.querySelector('textarea')
+  return textarea.value
+}
